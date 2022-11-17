5dccb68975f2654511af0d62e4c916f14d10fe45
Reason Adhikari <72090971+adhikariReason@users.noreply.github.com>
2022-11-16 23:18:55 -0600

    added delete function

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -109,3 +109,29 @@ exports.icecream_create_Page = function(req, res) {
 	res.send(`{'error': '${err}'}`);
 	}
    };
+// Handle building the view for updating a icecream.
+// query provides the id
+exports.icecream_update_Page = async function(req, res) {
+	console.log("update view for item "+req.query.id)
+	try{
+	let result = await Icecream.findById(req.query.id)
+	res.render('icecreamupdate', { title: 'Icecream Update', toShow: result });
+	}
+	catch(err){
+	res.status(500)
+	res.send(`{'error': '${err}'}`);
+	}
+   };
+// Handle a delete one view with id from query
+exports.icecream_delete_Page = async function(req, res) {
+	console.log("Delete view for id " + req.query.id)
+	try{
+	result = await Icecream.findById(req.query.id)
+	res.render('icecreamdelete', { title: 'Icecream Delete', toShow:
+   result });
+	}
+	catch(err){
+	res.status(500)
+	res.send(`{'error': '${err}'}`);
+	}
+   };
--- routes/icecream.js
+++ routes/icecream.js
@@ -10,4 +10,10 @@ router.get('/detail', icecream_controllers.icecream_view_one_Page);
 /* GET create icecream page */
 router.get('/create', icecream_controllers.icecream_create_Page);
 
+/* GET create update page */
+router.get('/update', icecream_controllers.icecream_update_Page);
+
+/* GET delete icecream page */
+router.get('/delete', icecream_controllers.icecream_delete_Page);
+
 module.exports = router;
